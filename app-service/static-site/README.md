1570c8bf610c4d7548bbc264014392286941a56d
Kenji Saito <ken-yo@mbr.nifty.com>
2022-11-12 16:42:11 +0900

    Add WebApp example

---
+++ app-service/static-site/README.md
@@ -0,0 +1,8 @@
+# Example for Web App Service
+
+## deploy
+
+```sh
+TF_VAR_githubToken=${GITHUB_TOKEN_FOR_AZURE_APP_DEPLOY}
+cdktf deploy
+```
---
+++ app-service/static-site/src/main.ts
@@ -0,0 +1,42 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import { Construct } from "constructs";
+import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
+import { ResourceGroup } from "@cdktf/provider-azurerm/lib/resource-group";
+import { StaticSite } from "@cdktf/provider-azurerm/lib/static-site";
+
+class ExampleWebApp extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new AzurermProvider(this, "azureFeature", {
+      features: {},
+    });
+
+    const rg = new ResourceGroup(this, "cdktf-rg", {
+      name: "poad-cdktf-demo-rg",
+      location: "centralus",
+    });
+
+    const app = new StaticSite(this, "cdktf-app", {
+      name: "poad-cdktf-demo-app",
+      location: rg.location,
+      resourceGroupName: rg.name,
+      skuTier: 'Free',
+      skuSize: 'Free'
+    });
+
+    new TerraformOutput(this, "cdktf-app-apikey", {
+      value: app.apiKey,
+    });
+
+    new TerraformOutput(this, "cdktf-app-url", {
+      value: `https://${app.name}.azurewebsites.net/`,
+    });
+  }
+}
+
+const app = new App();
+new ExampleWebApp(app, "webapp");
+app.synth();
