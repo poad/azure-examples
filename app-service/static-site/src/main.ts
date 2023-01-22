d431e8745313ded1ce8453bfa73e5063fb199f7c
Kenji Saito <ken-yo@mbr.nifty.com>
2023-01-22 21:43:03 +0900

    Add the deploy workflow

--- app-service/static-site/src/main.ts
+++ app-service/static-site/src/main.ts
@@ -1,7 +1,5 @@
-// Copyright (c) HashiCorp, Inc
-// SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
-import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { App, TerraformStack } from "cdktf";
 import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
 import { ResourceGroup } from "@cdktf/provider-azurerm/lib/resource-group";
 import { StaticSite } from "@cdktf/provider-azurerm/lib/static-site";
@@ -19,16 +17,12 @@ class ExampleWebApp extends TerraformStack {
       location: "centralus",
     });
 
-    const app = new StaticSite(this, "cdktf-app", {
+    new StaticSite(this, "cdktf-app", {
       name: "poad-cdktf-demo-app",
       location: rg.location,
       resourceGroupName: rg.name,
       skuTier: 'Free',
       skuSize: 'Free'
-    });
-
-    new TerraformOutput(this, "cdktf-app-apikey", {
-      value: app.apiKey,
     });
   }
 }
